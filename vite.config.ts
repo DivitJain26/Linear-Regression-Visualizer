848691abcf7a6d17650852a1aa0cf4a0ff0673ca
Divit <divit26j@gmail.com>
2026-03-12 13:41:53 +0000

    pages added

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: '/Linear-Regression-Visualizer/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
